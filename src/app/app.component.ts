646a55e4b764fe86203711f739ea325ac348fde3
Akshita13 <akshitakapadia13@gmail.com>
2020-06-26 16:24:26 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,40 @@
 import { Component, VERSION } from "@angular/core";
-import { FormControl } from "@angular/forms";
+import { FormControl, FormBuilder, FormGroup } from "@angular/forms";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  public tagIds: FormControl = new FormControl("");
+  userFormGroup;
+  tagArray: any[] = [];
+  constructor(private fb: FormBuilder) {
+    this.userFormGroup = this.buildForm();
+  }
+  public buildForm(): FormGroup {
+    return this.fb.group({
+      tagIds: [""]
+    });
+  }
+  // public tagIds: FormControl = new FormControl("");
   tags = [
     { tagId: 1, tagName: "JAVA" },
     { tagId: 2, tagName: "CEO" },
     { tagId: 3, tagName: "QA" },
     { tagId: 4, tagName: "ANGLAR" }
   ];
+  addTagDetail = (name: string): string => this.addNew(name);
+
+  addNew(value) {
+   
+    // let tagId = this.tags.length + 1;
+    // let obj = { tagId: tagId, tagName: value };
+    // this.tags.push(obj);
+    // console.log(this.tags)
+    return value;
+  }
+
+  save() {
+    console.log(this.userFormGroup.controls.tagIds.value);
+  }
 }
