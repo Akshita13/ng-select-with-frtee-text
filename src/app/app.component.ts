34648dce3f5d9270d4ac2a9264249bc9da22d99e
Akshita13 <akshitakapadia13@gmail.com>
2020-06-29 13:09:18 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,28 +13,18 @@ export class AppComponent {
   }
   public buildForm(): FormGroup {
     return this.fb.group({
-      tagIds: [""]
+      tagName: [""]
     });
   }
-  
-  tags = [
-    { tagId: 1, tagName: "JAVA" },
-    { tagId: 2, tagName: "CEO" },
-    { tagId: 3, tagName: "QA" },
-    { tagId: 4, tagName: "ANGLAR" }
-  ];
+
+  tags = ["JAVA", "CEO", "QA", "ANGLAR"];
   addTagDetail = (name: string): string => this.addNew(name);
 
   addNew(value) {
-   
-    // let tagId = this.tags.length + 1;
-    // let obj = { tagId: tagId, tagName: value };
-    // this.tags.push(obj);
-    // console.log(this.tags)
     return value;
   }
 
   save() {
-    console.log(this.userFormGroup.controls.tagIds.value);
+    console.log(this.userFormGroup.controls.tagName.value,"dfgfdgg");
   }
 }
